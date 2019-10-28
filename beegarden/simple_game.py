b1238834dd572f2da11b417d902d0b7d2d6a8d08
Rosin <asakhno@gmail.com>
2019-10-28 15:08:05 +0300

    Добавил if ... main для проверки модуля

--- beegarden/simple_game.py
+++ beegarden/simple_game.py
@@ -4,13 +4,14 @@
 from beegarden.core import Beegarden
 from beegarden.my_bee import MyBee
 
-scene = Beegarden(
-    name="My little garden",
-    flowers_count=10,
-    theme_mod_path='beegarden.themes.default',
-)
+if __name__ == '__main__':
+    scene = Beegarden(
+        name="My little garden",
+        flowers_count=10,
+        theme_mod_path='beegarden.themes.default',
+    )
 
-bee = MyBee()  # bee born
+    bee = MyBee()  # bee born
 
-scene.go()  # let the world revolves around you
+    scene.go()  # let the world revolves around you
 
