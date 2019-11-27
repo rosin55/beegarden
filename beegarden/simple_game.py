328c67279d998fc0989d1c69e6bcad15c9ca5f65
Rosin <asakhno@gmail.com>
2019-11-27 10:44:31 +0300

    Добавил свою тему

--- beegarden/simple_game.py
+++ beegarden/simple_game.py
@@ -7,10 +7,10 @@ from beegarden.my_bee import MyBee
 if __name__ == '__main__':
     scene = Beegarden(
         name="My little garden",
-        flowers_count=10,
-        theme_mod_path='beegarden.themes.default',
+        flowers_count=5,
+        theme_mod_path='beegarden.themes.for_test',
     )
-
     bee = MyBee()  # bee born
+    bee1 = MyBee()
 
     scene.go()  # let the world revolves around you
